a67a2fcd72ed7c3508e36e82277f68810269d544
spreyo <60149407+spreyo@users.noreply.github.com>
2022-06-25 14:20:50 +0200

    Add files via upload

---
+++ static/media/zoznam.efff23df85afd650870d.ml
@@ -0,0 +1,48 @@
+# Zoznam do hutníka
+
+ 
+
+ telefón motorola
+ 
+ telefón iPhone
+ 
+ nabíjačka na iPhone
+ 
+ nabíjačka na Motorolu
+ 
+ microUSB kabel
+ 
+ slúchadlá JBL
+ 
+ slúchadlá TWS BATTLETRON
+ 
+ rubik. kocky (x4)
+ 
+ notebook
+ 
+ nabíjačka na notebook
+ 
+ ceruzky/peračník
+ 
+ zápisník(x2)
+ 
+ okuliare
+ 
+ hodniky
+ 
+ fotoapapát
+ 
+ baterky do fotoapapátu
+ 
+ UNO
+ 
+ knižky (asi x2)
+ 
+ lopty
+ 
+ sedma
+ 
+ 
+ 
+ 
+ 
